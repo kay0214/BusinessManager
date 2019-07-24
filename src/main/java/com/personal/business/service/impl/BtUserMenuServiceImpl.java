56284f1e8214eb3116f3a2034cd99cd84953540b
sunpeikai <spk123456>
2019-07-24 10:49:26 +0800

    完成了，剩下的就配置一下菜单及权限

--- src/main/java/com/personal/business/service/impl/BtUserMenuServiceImpl.java
+++ src/main/java/com/personal/business/service/impl/BtUserMenuServiceImpl.java
@@ -1,11 +1,14 @@
 package com.personal.business.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.personal.business.entity.BtUserMenu;
 import com.personal.business.mapper.BtUserMenuMapper;
 import com.personal.business.request.PermissionsRequest;
 import com.personal.business.service.IBtUserMenuService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * <p>
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
  * @author spk
  * @since 2019-07-16
  */
+@Slf4j
 @Service
 public class BtUserMenuServiceImpl extends ServiceImpl<BtUserMenuMapper, BtUserMenu> implements IBtUserMenuService {
 
@@ -26,7 +30,19 @@ public class BtUserMenuServiceImpl extends ServiceImpl<BtUserMenuMapper, BtUserM
      */
     @Override
     public boolean authorize(PermissionsRequest request) {
-        return false;
+        String[] ids = splitString(request.getIds());
+        UpdateWrapper<BtUserMenu> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.lambda().eq(BtUserMenu::getUserId,request.getUserId()).in(BtUserMenu::getMenuId,ids);
+        getBaseMapper().delete(updateWrapper);
+        for(String id:ids){
+            if(!StringUtils.isEmpty(id)){
+                BtUserMenu btUserMenu = new BtUserMenu();
+                btUserMenu.setMenuId(Integer.valueOf(id));
+                btUserMenu.setUserId(request.getUserId());
+                save(btUserMenu);
+            }
+        }
+        return true;
     }
 
     /**
@@ -37,6 +53,19 @@ public class BtUserMenuServiceImpl extends ServiceImpl<BtUserMenuMapper, BtUserM
      */
     @Override
     public boolean unAuthorize(PermissionsRequest request) {
-        return false;
+        String[] ids = splitString(request.getIds());
+        UpdateWrapper<BtUserMenu> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.lambda().eq(BtUserMenu::getUserId,request.getUserId()).in(BtUserMenu::getMenuId,ids);
+        return getBaseMapper().delete(updateWrapper)>0;
+    }
+
+    /**
+     * @description 截取字符串，逗号截取 - lambda要求不能动态变量
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    private String[] splitString(String val){
+        return val.split(",");
     }
 }
