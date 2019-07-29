7951f39175a599f900751fef0840e52cc7e88cae
sunpeikai <spk123456>
2019-07-29 21:26:07 +0800

    数据字典

--- src/main/java/com/personal/business/controller/DictionaryController.java
+++ src/main/java/com/personal/business/controller/DictionaryController.java
@@ -3,15 +3,28 @@
  */
 package com.personal.business.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.personal.business.Enum.ResultEnum;
 import com.personal.business.base.BaseController;
 import com.personal.business.base.Return;
+import com.personal.business.constant.CommonConstant;
+import com.personal.business.dto.DictionaryExportDto;
+import com.personal.business.entity.BtDictionary;
 import com.personal.business.service.IBtDictionaryService;
+import com.personal.business.utils.CommonUtils;
+import com.personal.business.utils.EasyPoiUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author sunpeikai
@@ -35,5 +48,89 @@ public class DictionaryController extends BaseController {
     @ResponseBody
     public Return getAllDic(){
         return Return.data(iBtDictionaryService.getAllDictionary());
+    }
+
+    /**
+     * @description 更新字典
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    //@RequiresPermissions(ShiroPermissionsConstant.PERM_MENU_EDIT)
+    @PostMapping(value = "/update")
+    @ResponseBody
+    public Return update(@RequestBody BtDictionary dictionary){
+        log.info(JSON.toJSONString(dictionary));
+        boolean success = iBtDictionaryService.updateById(dictionary);
+        if(success){
+            return Return.success();
+        }else{
+            return Return.fail("更新失败");
+        }
+    }
+
+    /**
+     * @description 插入菜单
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    //@RequiresPermissions(ShiroPermissionsConstant.PERM_MENU_ADD)
+    @PostMapping(value = "/insert")
+    @ResponseBody
+    public Return insert(@RequestBody BtDictionary dictionary){
+        log.info("insert dictionary [{}]",JSON.toJSONString(dictionary));
+        dictionary.setDelFlag(0);
+        dictionary.setCreateTime(LocalDateTime.now());
+        dictionary.setUpdateTime(LocalDateTime.now());
+        boolean success = iBtDictionaryService.save(dictionary);
+        if(success){
+            return Return.success();
+        }else{
+            return Return.fail("添加失败");
+        }
+    }
+
+    /**
+     * @description 删除菜单
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    //@RequiresPermissions(ShiroPermissionsConstant.PERM_MENU_DEL)
+    @GetMapping(value = "/delete/{ids}")
+    @ResponseBody
+    public Return delete(@PathVariable String ids){
+        if(!StringUtils.isEmpty(ids)){
+            List<String> idsArray = new ArrayList<>();
+            Collections.addAll(idsArray, ids.split(","));
+            log.info("delete dictionary [{}]",idsArray);
+            boolean success = iBtDictionaryService.removeByIds(idsArray);
+            if(success){
+                return Return.success();
+            }else{
+                return Return.fail("删除失败");
+            }
+        }
+        return Return.fail(ResultEnum.ERROR_PARAM_NOT_ENOUGH);
+    }
+
+    /**
+     * @description 导出
+     * @auth sunpeikai
+     * @param
+     * @return
+     */
+    //@RequiresPermissions(ShiroConstants.ORDER_EXPORT_PERM)
+    @GetMapping("/exportExcel")
+    public void exportExcel(HttpServletResponse response) {
+        log.info("export excel");
+        List<BtDictionary> result = iBtDictionaryService.getAllDictionary();
+
+        if (CollectionUtils.isNotEmpty(result)){
+            List<DictionaryExportDto> exportDtos = CommonUtils.convertBeanList(result,DictionaryExportDto.class);
+            // 导出数据
+            EasyPoiUtil.exportExcel(exportDtos,"数据字典","第一页", DictionaryExportDto.class,"数据字典.xls",response) ;
+        }
     }
 }
---
+++ src/main/java/com/personal/business/dto/DictionaryExportDto.java
@@ -0,0 +1,86 @@
+/*
+ * @Copyright: 2005-2018 www.hyjf.com. All rights reserved.
+ */
+package com.personal.business.dto;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author sunpeikai
+ * @version DictionaryExportDto, v0.1 2019/7/29 21:09
+ * @description
+ */
+@Data
+@ExcelTarget("BtDictionary")
+public class DictionaryExportDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Excel(name = "ID" , width = 20)
+    private Integer id;
+
+    /**
+     * 字典区分,用来区分某一类字典
+     */
+    @Excel(name = "字典区分" , width = 20)
+    private String type;
+
+    /**
+     * 字典值
+     */
+    @Excel(name = "字典值" , width = 20)
+    private Integer value;
+
+    /**
+     * 字典名称
+     */
+    @Excel(name = "字典名称" , width = 20)
+    private String name;
+
+    /**
+     * 排序字段
+     */
+    @Excel(name = "排序字段" , width = 20)
+    private Integer orderNum;
+
+    /**
+     * 父节点id
+     */
+    @Excel(name = "父节点id" , width = 20)
+    private Integer parentId;
+
+    /**
+     * 冻结（0：未冻结，1：冻结）
+     */
+    @Excel(name = "状态" , width = 20 , replace = {"可用_0", "冻结_1"})
+    private Integer freeze;
+
+    /**
+     * 是否已用（0：未用，1：已用）
+     */
+    @Excel(name = "已使用" , width = 20 , replace = {"未用_0", "已用_1"})
+    private Integer used;
+
+    /**
+     * 是否内置（0：否，1：是）
+     */
+    @Excel(name = "内置" , width = 20 , replace = {"否_0", "是_1"})
+    private Integer builtIn;
+
+    /**
+     * 暂时保留吧
+     */
+    private Integer nodeTypeId;
+
+    /**
+     * 描述
+     */
+    @Excel(name = "描述" , width = 20)
+    private String remark;
+
+}
